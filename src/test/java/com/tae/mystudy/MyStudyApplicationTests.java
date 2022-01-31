c88f2b5970c8d16ea4fbf72b4e5918ff8bbc860e
T.H Kim <zppy13@gmail.com>
2022-01-31 18:41:18 +0900

    Initial commit

---
+++ src/test/java/com/tae/mystudy/MyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tae.mystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
