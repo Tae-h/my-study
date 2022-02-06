3f8ef355093ec8ffc191312a6d406dda9426c693
T.H Kim <zppy13@gmail.com>
2022-02-06 16:42:04 +0900

    [Programers] 해쉬 -> 위장(경우의 수)

--- src/main/java/com/taehoon/codingtest/programers/solutions/HashSolutions.java
+++ src/main/java/com/taehoon/codingtest/programers/solutions/HashSolutions.java
@@ -2,6 +2,8 @@ package com.taehoon.codingtest.programers.solutions;
 
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 해시 문제
@@ -17,8 +19,8 @@ public class HashSolutions {
 
         //System.out.println("answer:: " + completions[3]);
 
-        String remains = notFinishedPlayer(participants, completions);
-        System.out.println(remains);
+//        String remains = notFinishedPlayer(participants, completions);
+//        System.out.println(remains);
 
         /* 전화번호 목록 예시
             ["119", "97674223", "1195524421"]	false
@@ -27,12 +29,21 @@ public class HashSolutions {
          */
         //String[] phoneBook = {"119", "97674223", "1195524421"};
         //String[] phoneBook = {"123","456","789"};
-        String[] phoneBook = {"12","123","1235","567","88"};
-        phoneNumberList(phoneBook);
+        //String[] phoneBook = {"12","123","1235","567","88"};
+       // phoneNumberList(phoneBook);
 
+        /*
+        * 위장
+        * [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]	5
+           [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]	3
+        * */
+        String[][] clothes = { // return 5
+                {"yellowhat", "headgear"},
+                {"bluesunglasses", "eyewear"},
+                {"green_turban", "headgear"}
+        };
 
-
-
+        camouflage(clothes);
     }
 
     /*  완주하지 못한선수 */
@@ -72,6 +83,27 @@ public class HashSolutions {
 
         return answer;
     }
+
+    /* 위장 */
+    public static int camouflage(String[][] clothes) {
+
+        Map<String, Integer> clotheMap = new HashMap<>();
+        for ( String[] clothe : clothes ) {
+            clotheMap.put(clothe[1], clotheMap.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        int answer = 1;
+        // values 1, 2
+        for ( Integer value : clotheMap.values() ) {
+            answer *= (value + 1); // (n + 1) * (r + 1) 서로 택 하지 않는 경우 도 있어서 +1 해줘야 함
+        }
+
+        /* 아무것도 선택 하지 않을 경우 */
+        answer -= 1;
+
+        return answer;
+    }
+
 
 
 
