f0b5fea4174d07e503f0abdd0c88f1ddda61488b
T.H Kim <zppy13@gmail.com>
2022-02-10 17:17:40 +0900

    [Programers] 정렬 -> 가장 큰수 작업중...

--- src/main/java/com/taehoon/codingtest/programers/solutions/SortSolutions.java
+++ src/main/java/com/taehoon/codingtest/programers/solutions/SortSolutions.java
@@ -10,8 +10,9 @@ public class SortSolutions {
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
         //return  {5, 6, 3};
 
-        kthNumber(array, commands);
-
+        //kthNumber(array, commands);
+        int [] numbers = {6, 10, 2}; // return "6210"
+        biggestNumber(numbers);
     }
 
     /* k번째 수 */
@@ -29,5 +30,23 @@ public class SortSolutions {
         return answer;
     }
 
+
+    /* 가장 큰 수 */
+    public static String biggestNumber(int[] numbers) {
+        String answer = "";
+
+        Integer[] arr = new Integer[numbers.length];
+
+        for ( int i = 0; i < numbers.length; i++ ) {
+            arr[i] = numbers[i];
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        
+
+
+
+        return answer;
+    }
 
 }
